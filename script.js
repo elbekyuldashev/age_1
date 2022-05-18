40736fe9650d5bbd6df209f6f6f005a9b7fff23b
first_name last_name <my_name@example.com>
2022-05-18 13:15:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let age = +prompt('age')
+
+
+if(age  < 10) {
+    alert("baby");
+}
+else if (age < 20) {
+    alert("teenager");
+}
+else if (age < 30) {
+    alert("it's time to get married");
+}
+else if (age < 50) {
+    alert("old man");
+}
+else if (age < 70) {
+    alert("it's time for retirement");
+}
+else if (age < 100) {
+    alert("lie down and have fun");
+}
+else {
+    alert("goodbaye")
+}
